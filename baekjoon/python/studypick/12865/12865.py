307dd53a52cab2acd8760f4a2594c7574bad2713
shynewsky <shynewsky@gmail.com>
2026-01-11 09:20:14 +0900

    .

---
+++ baekjoon/python/studypick/12865/12865.py
@@ -0,0 +1,27 @@
+import sys
+sys.stdin = open('input.txt')
+
+# 함수 ----------------------------------------------------
+
+def recur(n):
+
+    if n == N:
+        return
+
+    for _ in range():
+        return
+
+
+
+
+# 입력 -----------------------------------------------------
+
+N, K = map(int, input().split())
+data = [list(map(int, input().split())) for _ in range(N)]
+
+# 코드 -------------------------
+
+recur(0)
+
+
+
