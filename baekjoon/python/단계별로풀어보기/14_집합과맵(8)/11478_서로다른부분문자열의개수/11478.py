0b8051307d964fde3327bbe7e77b7e5d177fb36e
shynewsky <shynewsky@gmail.com>
2026-01-13 17:50:38 +0900

    .

--- baekjoon/python/단계별로풀어보기/14_집합과맵(8)/11478_서로다른부분문자열의개수/11478.py
+++ baekjoon/python/단계별로풀어보기/14_집합과맵(8)/11478_서로다른부분문자열의개수/11478.py
@@ -2,5 +2,13 @@ import sys
 sys.stdin = open('input.txt')
 
 S = input()
-l = len(S)
-print(l)
+li = []
+cnt = 0
+for start_idx in range(0, len(S)):
+    for end_idx in range(start_idx+1, len(S)+1):
+        string = S[start_idx:end_idx]
+        if string not in li:
+            li.append(string)
+            cnt += 1
+
+print(cnt)
