887360a6e412da557ff74890ea774f3a98fb8597
shynewsky <shynewsky@gmail.com>
2026-01-14 21:55:28 +0900

    260114

--- programmers/java/기초124/18.5_my_string/18_5.java
+++ programmers/java/기초124/18.5_my_string/18_5.java
@@ -2,6 +2,6 @@ class Solution {
     public String solution(String rny_string) {
         // String answer = "";
         // return answer;
-        return rny_string.replace('m', 'rn');
+        return rny_string.replace("m", "rn");
     }
 }
