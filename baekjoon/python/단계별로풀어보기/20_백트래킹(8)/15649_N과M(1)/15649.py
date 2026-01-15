a47daf4d079fd8c003c06c74e7a89fa3884c2e30
shynewsky <shynewsky@gmail.com>
2026-01-15 09:25:33 +0900

    .

---
+++ baekjoon/python/단계별로풀어보기/20_백트래킹(8)/15649_N과M(1)/15649.py
@@ -0,0 +1,14 @@
+import sys
+sys.stdin = open('input.txt')
+
+def back(n, path):
+    if n == N:
+        return
+    
+    
+
+
+N, M = map(int, input().split())
+back(0, [])
+
+
---
+++ programmers/java/기초124/17.5_ad제거하기/17_5.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] strArr) {
+        // String[] answer = {};
+        // return answer;
+        List<String> list = new ArrayList<>();
+
+        for (String s : strArr) {
+            if (!s.contains("ad")) {
+                list.add(s);
+            }
+        }
+
+        return list.toArray(new String[0]);
+    }
+}
