e108b790d2d2d1d6537008249a1d976d1dac9771
shynewsky <shynewsky@gmail.com>
2026-01-18 19:24:31 +0900

    l.l

--- baekjoon/python/단계별로풀어보기/20_백트래킹(8)/15649_N과M(1)/15649.py
+++ baekjoon/python/단계별로풀어보기/20_백트래킹(8)/15649_N과M(1)/15649.py
@@ -1,14 +1,25 @@
 import sys
 sys.stdin = open('input.txt')
 
-def back(n, path):
-    if n == N:
+# 함수 -----------------------------
+def f(n):
+    if n == M:
+        print(*path)
         return
-    
-    
 
+    for i in range(1, N+1):
+        if visited[i] == 1:
+            continue
+        visited[i] = 1
+        path.append(i)
+        f(n+1)
+        path.pop()
+        visited[i] = 0
 
+# 입력 ------------------------------
 N, M = map(int, input().split())
-back(0, [])
 
-
+# 코드 ------------------------------
+path = []
+visited = [0] * (N+1)
+f(0)
