2620edf37c2375a1f8e5cddb27ac3d9725d70719
shynewsky <shynewsky@gmail.com>
2026-01-20 23:59:16 +0900

    .

--- baekjoon/python/단계별로풀어보기/21_동적계획법1(16)/1149_RGB거리/1149.py
+++ baekjoon/python/단계별로풀어보기/21_동적계획법1(16)/1149_RGB거리/1149.py
@@ -6,21 +6,20 @@ import sys
 sys.stdin = open('input.txt')
 input = sys.stdin.readline
 write = sys.stdout.write
+from pprint import pprint
 # ---------------------------------
-
-
-
-
-
 
 
 
 # ---------------------------------
 # 입출력
 N = int(input())
-mat = [list(map(int, input().split)) for _ in range(N)]
+mat = [list(map(int, input().split())) for _ in range(N)]
+pprint(mat)
 
 
 
 
-
+# 타이머
+end = time.time()
+print(str(end-start) + '초\n')
