1a127c7b9794a8de41f50af09eade9c829ace475
shynewsky <shynewsky@naver.com>
2026-02-19 16:41:38 +0900

    백준 11866

--- baekjoon/python/solved.ac/class1/11866_요세푸스문제0/11866.py
+++ baekjoon/python/solved.ac/class1/11866_요세푸스문제0/11866.py
@@ -6,6 +6,20 @@ N, K = map(int, input().split())
 start = [i for i in range(1, N+1)]
 answer = []
 
+i = K-1
+while start:
 
+    answer.append(start[i])
+    start[i] = 0
 
+    i += K
+    while i >= N :
+        i -= N
+        cnt = start.count(0)
+        for _ in range(cnt):
+            start.remove(0)
+        if not start:
+            break
+        N = len(start)
 
+print(f"<{', '.join(map(str, answer))}>")
