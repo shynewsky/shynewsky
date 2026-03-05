adc4afbd005b433998358a233f406bd172700573
shynewsky <shynewsky@gmail.com>
2026-03-05 12:54:26 +0900

    .

--- baekjoon/python/solved.ac/class2/1018_체스판다시칠하기/1018.py
+++ baekjoon/python/solved.ac/class2/1018_체스판다시칠하기/1018.py
@@ -7,15 +7,50 @@ input = sys.stdin.readline
 from pprint import pprint
 
 N, M = map(int, input().split()) # 8~50 자연수
-mat = [list(input().strip()) for _ in range(M)]
+board = [input().strip() for _ in range(M)]
+
+board_white = [
+    'WBWBWBWB',
+    'BWBWBWBW',    
+    'WBWBWBWB',
+    'BWBWBWBW',    
+    'WBWBWBWB',
+    'BWBWBWBW',    
+    'WBWBWBWB',
+    'BWBWBWBW',    
+]
+board_black = [
+    'BWBWBWBW',
+    'WBWBWBWB',
+    'BWBWBWBW',
+    'WBWBWBWB',
+    'BWBWBWBW',
+    'WBWBWBWB',
+    'BWBWBWBW',
+    'WBWBWBWB',
+]
 
 for i in range(N):
     for j in range(M):
+        first = board[i][j]
+        wrong = False
 
-
-
-
-
+        if first == 'W':
+            for k in range(8):
+                for l in range(8):
+                    if board[i+k][j+l] != board_white[k][l]:
+                        wrong = True
+                        break
+                if wrong:
+                    break
+        elif first == 'B':
+            for k in range(8):
+                for l in range(8):
+                    if board[i+k][j+l] != board_black[k][l]:
+                        wrong = True
+                        break
+                if wrong:
+                    break
 
 # -------------------
 end = time.time()
