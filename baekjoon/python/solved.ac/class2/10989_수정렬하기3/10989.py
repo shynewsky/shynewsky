eca395ae54c4ee98a757ea67f706f9b6f2d2e7f6
shynewsky <shynewsky@gmail.com>
2026-04-27 12:45:06 +0900

    .

--- baekjoon/python/solved.ac/class2/10989_수정렬하기3/10989.py
+++ baekjoon/python/solved.ac/class2/10989_수정렬하기3/10989.py
@@ -1,15 +1,12 @@
 import sys
 sys.stdin = open('input.txt')
 
-
-
-10,000,000
-
+count = [0] * 10001
 
 N = int(input())
-arr = [0] * N
 for i in range(N):
-    arr[i] = int(input())
-arr.sort()
-for a in arr:
-    print(a)
+    num = int(input())
+    count[num] += 1
+for i in range(10000000):
+    for j in range(count[i]):
+        print(i)
